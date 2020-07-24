09632721ef08c1f69a7759e77f3761ee6b6300a4
72nd <32248252+72nd@users.noreply.github.com>
2020-07-24 23:49:48 +0200

    qr code content done

--- invoice.go
+++ invoice.go
@@ -1,18 +1,30 @@
 package swiss_qr_invoice
 
-import "github.com/creasty/defaults"
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"text/template"
+
+	"github.com/creasty/defaults"
+)
 
 // Invoice contains all necessary information for the generation of an invoice.
 type Invoice struct {
 	ReceiverIBAN    string `yaml:"receiver_iban" default:"CH44 3199 9123 0008 8901 2"`
+	IsQrIBAN bool `yaml:"is_qr_iban" default:"true"`
 	ReceiverName    string `yaml:"receiver_name" default:"Robert Schneider AG"`
-	ReceiverAddress string `yaml:"receiver_address" default:"Rue du Lac 1268"`
+	ReceiverStreet  string `yaml:"receiver_street" default:"Rue du Lac"`
+	ReceiverNumber  string `yaml:"receiver_number" default:"12"`
 	ReceiverZIPCode string `yaml:"receiver_zip_code" default:"2501"`
 	ReceiverPlace   string `yaml:"receiver_place" default:"Biel"`
+	ReceiverCountry string `yaml:"receiver_country" default:"CH"`
 	PayeeName       string `yaml:"receiver_name" default:"Pia-Maria Rutschmann-Schnyder"`
-	PayeeAddress    string `yaml:"receiver_address" default:"Grosse Marktgasse 28"`
+	PayeeStreet     string `yaml:"receiver_street" default:"Grosse Marktgasse"`
+	PayeeNumber     string `yaml:"receiver_number" default:"28"`
 	PayeeZIPCode    string `yaml:"receiver_zip_code" default:"9400"`
 	PayeePlace      string `yaml:"receiver_place" default:"Rorschach"`
+	PayeeCountry    string `yaml:"payee_country" default:"CH"`
 	Reference       string `yaml:"reference" default:"21 00000 00003 13947 14300 09017"`
 	AdditionalInfo  string `yaml:"additional_info" default:"Rechnung Nr. 3139 für Gartenarbeiten"`
 	Amount          string `yaml:"amount" default:"3949.75"`
@@ -37,5 +49,59 @@ func (i Invoice) SaveAsPDF(path string) error {
 
 // noPayee returns true if no fields of the payee are set
 func (i Invoice) noPayee() bool {
-	return i.PayeeName == "" && i.PayeeAddress == "" && i.PayeeZIPCode == "" && i.PayeePlace == ""
+	return i.PayeeName == "" && i.PayeeStreet == "" && i.PayeeZIPCode == "" && i.PayeePlace == ""
+}
+
+func (i Invoice) qrContent() (string, error) {
+	qrTpl := `
+SPC
+0200
+1
+{{ .iban }}
+S
+{{ .inv.ReceiverName }}
+{{ .inv.ReceiverStreet }}
+{{ .inv.ReceiverNumber }}
+{{ .inv.ReceiverZIPCode }}
+{{ .inv.ReceiverPlace }}
+{{ .inv.ReceiverCountry }}
+
+
+
+
+
+
+
+{{ .inv.Amount }}
+{{ .inv.Currency }}
+S
+{{ .inv.PayeeName }}
+{{ .inv.PayeeStreet }}
+{{ .inv.PayeeNumber }}
+{{ .inv.PayeeZIPCode }}
+{{ .inv.PayeePlace }}
+{{ .inv.PayeeCountry }}
+{{ .refType }}
+{{ .inv.Reference }}
+{{ .inv.AdditionalInfo }}
+EPD
+`
+	refType := "QRR"
+	if !i.IsQrIBAN {
+		refType = "SCOR"
+	}
+	data := map[string]interface{}{
+		"inv":     i,
+		"iban":    strings.Replace(i.ReceiverIBAN, " ", "", -1),
+		"refType": refType,
+	}
+	tpl, err := template.New("qr-content").Parse(qrTpl)
+	if err != nil {
+		return "", fmt.Errorf("error while creating qr template: %s", err)
+	}
+	var buf bytes.Buffer
+	if err := tpl.Execute(&buf, data); err != nil {
+		return "", fmt.Errorf("error while applying data to qr template: %s", err)
+	}
+	return buf.String(), nil
 }
